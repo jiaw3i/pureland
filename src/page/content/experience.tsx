5b4781a718a975cf22825a4f8ef6ba28dd622858
hanjiawei <hjw01509608@alibaba-inc.com>
2022-09-27 10:47:36 +0800

    content

--- src/page/content/experience.tsx
+++ src/page/content/experience.tsx
@@ -1,6 +1,26 @@
 import experience from "./experience.less";
+import {useState} from "react";
 
-export default function Experience() {
+type ExperienceProps = {
+    id: string;
+    title: string;
+    company: string;
+    startDate: string;
+    endDate: string;
+    description: string;
+    job: string;
+}
+
+export default function Experience(props: ExperienceProps) {
+    const [item, setItem] = useState<ExperienceProps>({
+        id: props.id,
+        title: props.title,
+        company: props.company,
+        startDate: props.startDate,
+        endDate: props.endDate,
+        description: props.description,
+        job: props.job
+    });
     return (
         <div className={experience.plExperience}>
 
