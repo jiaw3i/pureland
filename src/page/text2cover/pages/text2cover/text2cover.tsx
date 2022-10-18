c6d6b5688a72670cd80e731b2f478efd32938ddf
hanjiawei <hanjw_cn@qq.com>
2022-10-18 23:09:41 +0800

    feat: 文字曲线

--- src/page/text2cover/pages/text2cover/text2cover.tsx
+++ src/page/text2cover/pages/text2cover/text2cover.tsx
@@ -15,6 +15,7 @@ export default function TitleConversion() {
     const [title, setTitle] = useState("");
 
     const [gradientStyle, setGradientStyle] = useState("");
+    const [fontSize, setFontSize] = useState(0);
 
     /**
      * 给title复制
@@ -36,10 +37,21 @@ export default function TitleConversion() {
         }, 1000);
     }
 
+    const calculateMaxFontSize = (width:number,height:number) => {
+        let titleLength = title.length;
+
+        let maxFontSize = width / titleLength;
+        return maxFontSize > height ? height : maxFontSize;
+
+    }
+
     const nextStyle = () => {
         // 生成随机的渐变色
         // 1-14256之间的随机数
         let random = Math.floor(Math.random() * 14256 + 1) + "";
+        // 生成随机字体大小
+        let randomFontSize = Math.floor(Math.random() * calculateMaxFontSize(512,200) + 1) + "";
+        setFontSize(parseInt(randomFontSize));
         randomGradientColors(random).then((res) => {
             console.log("random", res[0].gradient.split(":")[1].replace(";", ""));
             setGradientStyle(res[0].gradient.split(":")[1].replace(";", ""));
@@ -54,9 +66,23 @@ export default function TitleConversion() {
                 <Button onClick={conversionOnClick} size={"large"} type={"primary"}>转换</Button>
             </div>
             <div className={styles.titleConversionOutput}>
-                <div style={{background: gradientStyle}} className={styles.titleConversionOutputPreview}>
+                <div style={{
+                    background: gradientStyle,
+                    fontSize: fontSize,
+                }} className={styles.titleConversionOutputPreview}>
                     <div className={styles.titleConversionOutputTitle}>
-                        {title}
+                        <svg  width={512} height={200}>
+                            <path id={"path1"}
+                                  fill={"none"}
+                                  d="m79.5,79.8c2.29221,0 2.62296,2.11198 4.58442,6.78261c2.77392,6.60527 7.18585,15.63238 11.46104,22.6087c14.85706,24.24401 25.10016,38.52577 34.38312,49.73913c11.32295,13.67756 28.65814,30.15889 32.09091,33.91304c20.01631,21.89037 33.79263,24.05324 55.01299,31.65217c15.68572,5.61702 41.28779,8.04327 59.5974,9.04348c20.72507,1.13216 59.5974,2.26087 84.81169,2.26087c25.21428,0 61.88961,-2.26087 68.76623,-4.52174l2.29221,0"
+                                  stroke={"#000000"}
+                            />
+                            <text>
+                                <textPath href={"#path1"}>
+                                    {title}
+                                </textPath>
+                            </text>
+                        </svg>
                     </div>
 
                 </div>
