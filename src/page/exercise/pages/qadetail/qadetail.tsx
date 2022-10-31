e95539f4adb695439c5e22c0dac7e9ba14067666
hanjiawei <hanjw_cn@qq.com>
2022-10-31 22:27:39 +0800

    feat: 问答详情组件创建

---
+++ src/page/exercise/pages/qadetail/qadetail.tsx
@@ -0,0 +1,11 @@
+import styles from "./qadetail.less"
+
+export default function QADetail() {
+    return (
+        <div className={styles.qaDetail}>
+            <div className={styles.qaDetailTitle}>
+                <h1>QADetail</h1>
+            </div>
+        </div>
+    )
+}
